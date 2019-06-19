e8de5b8554223a0e2fd151c49a1bc0fadf01277f
mulholio <james@jmulholland.com>
2019-06-19 11:29:40 +0100

    add alternate import form fail

--- loadPolyfills.js
+++ loadPolyfills.js
@@ -7,4 +7,25 @@ async function loadPolyfills() {
   global.Intl = Intl
 }
 
+/*
+The following also fails in the same way:
+
+function loadPolyfills() {
+  return new Promise(resolve => {
+    if (!global.Intl) {
+      require.ensure([
+        'intl',
+        'intl/locale-data/jsonp/en.js'
+      ], function (require) {
+        require('intl');
+        require('intl/locale-data/jsonp/en.js');
+        resolve();
+      });
+    } else {
+      resolve();
+    }
+  })
+}
+*/
+
 export default loadPolyfills;
